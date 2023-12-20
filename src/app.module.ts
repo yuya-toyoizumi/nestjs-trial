6cc1bbc337fd44911064c3ec6fa5af2e09c1535b
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-20 12:56:51 +0900

    nest g resource dogs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [],
+  imports: [DogsModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
