9e35dcf57816290a6d089274a808ec9b8ac6dc03
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-22 21:32:37 +0900

    add api tags for openapi

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('cats')
+@ApiTags('cats')
 export class CatsController {
   @Get()
   findAll(): string {
--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { DogsService } from './dogs.service';
 import { CreateDogDto } from './dto/create-dog.dto';
 import { UpdateDogDto } from './dto/update-dog.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('dogs')
+@ApiTags('dogs')
 export class DogsController {
   constructor(private readonly dogsService: DogsService) {}
 
