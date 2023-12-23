adcc4f60aa9558ecbf89abaf4ec0a62872929b36
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-23 15:29:27 +0900

    set whitelist and forbidNonWhitelisted to true

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
 
   await app.listen(3000);
 }
