3a65e88ec38b494baf5ee13e892023e4b27d35b0
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-23 21:25:17 +0900

    do not expose swagger in production

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('nestjs-trial')
-    .setDescription('This is NestJS trial.')
-    .setVersion('1.0.0')
-    .addTag('trial')
-    .build()
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('nestjs-trial')
+      .setDescription('This is NestJS trial.')
+      .setVersion('1.0.0')
+      .addTag('trial')
+      .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document)
+  }
 
   await app.listen(3000);
 }
