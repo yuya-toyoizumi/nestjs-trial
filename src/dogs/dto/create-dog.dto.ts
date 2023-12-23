d1c9117636982e49ad1aa74cb0e354f2765bb709
yuya-toyoizumi <gbr.eng123@gmail.com>
2023-12-23 15:00:11 +0900

    add api property decorator

--- src/dogs/dto/create-dog.dto.ts
+++ src/dogs/dto/create-dog.dto.ts
@@ -1,6 +1,11 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDogDto {
   @IsString()
+  @ApiProperty({
+    description: 'The name of a dog',
+    default: 'shiba inu',
+  })
   name: string;
 }
